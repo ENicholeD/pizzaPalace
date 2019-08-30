ae8cd37a80b33e351086be4e41a1a6e55c1ae755
Erin Deuel <end.gaia@gmail.com>
2019-08-30 13:55:54 -0700

    fix the display of the price

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,30 @@
 function CustomPizza(toppings, price){
   this.toppings = toppings;
-  this.price = price;
+  this.price = 7;
 }
+
 CustomPizza.prototype.pizzaPrice = function(toppingNumber){
-  this.total = toppingNumber += this.price;
+  this.price += toppingNumber;
 }
 
 $(document).ready(function(){
-
+  $("#finishedPizza").hide();
   $(".form").submit(function(event){
+    event.preventDefault();
     var crust = $("input:radio[name=crust]:checked").val();
     var sauce = $("input:radio[name=sauce]:checked").val();
-    event.preventDefault();
-    var newPizza = new CustomPizza(allToppings);
-    if (crust, sauce){
-      var addOns = $("input:checkbox[name=topping]:checked").map(function(){ return this.value;}).get();
-      var allToppings = [];
-      (allToppings).push(addOns);
-      var toppingNumber = allToppings[0].length;
-      newPizza.addToppings;
-      newPizza.pizzaPrice;
-      $("#cost").text(newPizza.pice);
+    var addOns = $("input:checkbox[name=topping]:checked").map(function(){return this.value;}).get();
+    var allToppings = [];
+    (allToppings).push(addOns);
+    var toppingNumber = (allToppings[0]).length;
+    console.log(allToppings);
+    console.log(toppingNumber);
+    if(crust,sauce){
+    var newPizza = new CustomPizza(allToppings, toppingNumber);
+      newPizza.pizzaPrice(toppingNumber);
+
+      $("#cost").text(newPizza.price);
+      $("#finishedPizza").show();
     }
   });
 });
