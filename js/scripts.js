00692c72413963b55ab4734cddaffb3af2740ad4
Erin Deuel <end.gaia@gmail.com>
2019-09-03 21:19:05 -0700

    fix the front end and back end logic and the object with its prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,48 @@
-function CustomPizza(price){
+// Background logic
+
+function CustomPizza(crust, sauce, toppings, size){
+  this.crust = crust;
+  this.sauce = sauce;
+  this.toppings = toppings;
+  this.size = size;
   this.price = 2;
 }
-
-CustomPizza.prototype.pizzaPrice = function(toppingNumber){
-  this.price += toppingNumber;
+// adds addOns to the toppings on the CustomPizza
+CustomPizza.prototype.addPizza = function(addOns){
+  $(this.toppings).push(addOns);
 }
+// adds the price together to get the total
+CustomPizza.prototype.pizzaPrice = function(){
+  this.price += this.toppings.length;
+  this.price += this.size;
+}
+
+// UI logics
 
 $(document).ready(function(){
   $(".col-md-4").hide();
+  $("#total").hide();
+  $("#oops").hide();
   $(".form").submit(function(event){
     event.preventDefault();
-    var crust = $("input:radio[name=crust]:checked").val();
-    var sauce = $("input:radio[name=sauce]:checked").val();
+    // gathers all user input
+    var crustChoice = $("input:radio[name=crust]:checked").val();
+    var sauceChoice = $("input:radio[name=sauce]:checked").val();
     var addOns = $("input:checkbox[name=topping]:checked").map(function(){return this.value;}).get();
     var size = parseInt($("#size").val());
-    console.log(size);
-    var allToppings = [];
-    (allToppings).push(addOns);
-    var toppingNumber = (allToppings[0]).length;
-    console.log(allToppings);
-    console.log(toppingNumber);
-    if(crust,sauce){
-    var newPizza = new CustomPizza(allToppings, toppingNumber);
-      newPizza.pizzaPrice(toppingNumber);
-
-      $("#cost").text(newPizza.price + size);
+    // conditional
+    if(crustChoice, sauceChoice){
+    var newPizza = new CustomPizza(crustChoice, sauceChoice, addOns, size);
+      newPizza.addPizza();
+      newPizza.pizzaPrice();
+      $("#total").show();
+      $("oops").hide();
+      $("#cost").text(newPizza.price);
       $(".col-md-4").show();
+    } else {
+      $(".col-md-4").show();
+      $("#total").hide();
+      $("#oops").show();
     }
   });
 });
